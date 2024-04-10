f35e20abf99c6ecc737858dee0b2eb327c8237fc
Richard Sima <richard.sima2021@gmail.com>
2024-04-10 14:37:50 -0700

    implemented testData

---
+++ src/config/testData.js
@@ -0,0 +1,35 @@
+let testData = [
+    {
+        projectId: 1,
+        projectName: "Project 1",
+        projectDescription: "This is a test description",
+        projectStatus: "Ongoing",
+        teamMembers: ["Timmy", "Johnny", "Alice"],
+        startDate: "09-01-2024",
+        endDate: "",
+        projectLead: "Peter Anteater"
+        
+    },
+    {
+        projectId: 2,
+        projectName: "Project 2",
+        projectDescription: "This is a test description for project 2",
+        projectStatus: "Planning",
+        teamMembers: ["Michael", "Jason", "Bob"],
+        startDate: "09-01-2024",
+        endDate: "06-06-2025",
+        projectLead: "Peter Banteater"
+    },
+    {
+        projectId: 3,
+        projectName: "Project 3",
+        projectDescription: "This is a test description for project 3",
+        projectStatus: "Completed",
+        teamMembers: ["Harris", "Jones", "Jack"],
+        startDate: "04-20-2021",
+        endDate: "06-09-2022",
+        projectLead: "Peter Canteater"
+    },
+];
+
+export default testData;
--- src/pages/LandingPage/Landing.jsx
+++ src/pages/LandingPage/Landing.jsx
@@ -2,13 +2,18 @@ import React, { useEffect, useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import ProjectCard from "./components/ProjectCard";
 import { Wrap, WrapItem, Box } from "@chakra-ui/react";
+import testData from "../../config/testData";
 
 function Landing() {
-  let projects = [
-    { projectTitle: "Project 1", projectLeader: "Leader 1" },
-    { projectTitle: "Project 2", projectLeader: "Leader 2" },
-    { projectTitle: "Project 3", projectLeader: "Leader 3" },
-  ];
+  // let projects = [
+  //   { projectTitle: "Project 1", projectLeader: "Leader 1" },
+  //   { projectTitle: "Project 2", projectLeader: "Leader 2" },
+  //   { projectTitle: "Project 3", projectLeader: "Leader 3" },
+  // ];
+
+  let projects = testData;
+
+  console.log(projects);
 
   return (
     <div>
@@ -18,8 +23,8 @@ function Landing() {
           {projects.map((project, index) => (
             <WrapItem key={index}>
               <ProjectCard
-                projectTitle={project.projectTitle}
-                projectLeader={project.projectLeader}
+                projectTitle={project.projectName}
+                projectLeader={project.projectLead}
               />
             </WrapItem>
           ))}
