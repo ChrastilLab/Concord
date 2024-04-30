42368fc10b28c5578ab019fd457c86d64a3a8d42
Yukai Gu <yukaig1@uci.edu>
2024-04-29 22:07:08 -0700

    quick test of the keys

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -47,6 +47,13 @@ const firebaseConfig = {
     measurementId: process.env.REACT_APP_MEASUREMENT_ID
   };
   
+console.log(process.env.REACT_APP_FIREBASE_API_KEY);
+console.log(process.env.REACT_APP_AUTH_DOMAIN);
+console.log(process.env.REACT_APP_PROJECT_ID);
+console.log(process.env.REACT_APP_STORAGE_BUCKET);
+console.log(process.env.REACT_APP_MESSAGING_SENDER_ID);
+console.log(process.env.REACT_APP_APP_ID);
+console.log(process.env.REACT_APP_MEASUREMENT_ID);
 
 
 // Initialize Firebase
