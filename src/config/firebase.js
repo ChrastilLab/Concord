238a629b18001b96c6d5f86f08c94a58e3fed144
Yukai Gu <yukaig1@uci.edu>
2024-04-30 16:51:48 -0700

    test new deployment method with env var

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -45,15 +45,7 @@ const firebaseConfig = {
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_APP_ID,
     measurementId: process.env.REACT_APP_MEASUREMENT_ID
-  };
-  
-console.log(process.env.REACT_APP_FIREBASE_API_KEY);
-console.log(process.env.REACT_APP_AUTH_DOMAIN);
-console.log(process.env.REACT_APP_PROJECT_ID);
-console.log(process.env.REACT_APP_STORAGE_BUCKET);
-console.log(process.env.REACT_APP_MESSAGING_SENDER_ID);
-console.log(process.env.REACT_APP_APP_ID);
-console.log(process.env.REACT_APP_MEASUREMENT_ID);
+};
 
 
 // Initialize Firebase
