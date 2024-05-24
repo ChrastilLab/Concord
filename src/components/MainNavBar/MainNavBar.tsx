267bfc84020c006824390f861e46fe2e16af7235
Richard Sima <Richard.Sima2021@gmail.com>
2024-05-24 00:50:33 -0700

    basic functionality reimplemented via CRA with typescript

---
+++ src/components/MainNavBar/MainNavBar.tsx
@@ -0,0 +1,102 @@
+import React from "react";
+import { CircleUser, Menu, Brain, Search } from "lucide-react";
+import { Button } from "../ui/button";
+import { useNavigate, Link } from "react-router-dom";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import { Input } from "../ui/input";
+import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
+
+function MainNavBar() {
+  return (
+    <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+      <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+        <Link
+          to={"/"}
+          className="flex items-center gap-2 text-lg font-semibold md:text-base"
+        >
+          <Brain className="h-6 w-6" />
+          <span className="sr-only">Acme Inc</span>
+          Concord
+        </Link>
+        {/* <Link
+          href="/"
+          className="text-foreground transition-colors hover:text-foreground"
+        >
+          Home
+        </Link> */}
+        {/* <Link
+          href="/Project"
+          className="text-muted-foreground transition-colors hover:text-foreground"
+        >
+          Project
+        </Link> */}
+      </nav>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button variant="outline" size="icon" className="shrink-0 md:hidden">
+            <Menu className="h-5 w-5" />
+            <span className="sr-only">Toggle navigation menu</span>
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left">
+          <nav className="grid gap-6 text-lg font-medium">
+            <Link
+              to={"/"}
+              className="flex items-center gap-2 text-lg font-semibold"
+            >
+              <Brain className="h-6 w-6" />
+              <span className="sr-only">Acme Inc</span>
+              Concord
+            </Link>
+            {/* <Link href="/" className="hover:text-foreground">
+              Home
+            </Link>
+            <Link
+              href="/Project"
+              className="text-muted-foreground hover:text-foreground"
+            >
+              Project
+            </Link> */}
+          </nav>
+        </SheetContent>
+      </Sheet>
+      <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
+        <form className="ml-auto flex-1 sm:flex-initial">
+          <div className="relative">
+            <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+            <Input
+              type="search"
+              placeholder="Search project..."
+              className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
+            />
+          </div>
+        </form>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="secondary" size="icon" className="rounded-full">
+              <CircleUser className="h-5 w-5" />
+              <span className="sr-only">Toggle user menu</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Settings</DropdownMenuItem>
+            <DropdownMenuItem>Support</DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Logout</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </header>
+  );
+}
+
+export default MainNavBar;
