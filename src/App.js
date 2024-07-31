07fee8cbfce3dd3579c8d9f0471b91a7ce680e54
KainoaNishida <kanishid@uci.edu>
2024-07-30 19:26:30 -0700

    added a Login page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import { ChakraProvider} from "@chakra-ui/react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Studies from "./pages/Studies";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <ChakraProvider>
       <Router>
         <Routes>
+          
           <Route path="/" element={<Home/>}/>
+          <Route path="/Login" element={<Login/>}/>
           <Route path="/studies" element={<Studies/>}/>
         </Routes>
       </Router>
