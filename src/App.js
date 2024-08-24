a32cadb89fa34b63f4452bc68f9367f4ab561bbd
Aston Ki Chan <kic2@uci.edu>
2024-08-24 12:16:55 +0800

    Added RA Summary.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import { ChakraProvider} from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Studies from "./pages/Studies";
 import LabSheets from "./pages/LabSheet";
 import Table from "./pages/Table";
+import RASummary from "./pages/RASummary";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/studies" element={<Studies/>}/>
           <Route path="/labsheet" element={<LabSheets/>}/>
           <Route path="/announcements" element={<Table/>}/>
+          <Route path="/RASummary" element={<RASummary/>}/>
         </Routes>
       </Router>
     </ChakraProvider>
