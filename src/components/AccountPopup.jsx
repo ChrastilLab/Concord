537411e2f689c7b30c97ae0b58c46079910519ac
jessieh9 <104229166+jessieh9@users.noreply.github.com>
2024-09-02 19:14:37 -0700

    old code

--- src/components/AccountPopup.jsx
+++ src/components/AccountPopup.jsx
@@ -33,7 +33,7 @@ import {
 } from "recharts";
 import EditAccountInfo from "./EditAccountInfo";
 
-const userData = {
+export const userData = {
   avatarImg: "",
   name: "Yukai Gu",
   email: "yukai1@uci.edu",
