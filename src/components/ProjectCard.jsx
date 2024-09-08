b01532f8da21f6dcd1b36829007f9379245c7327
KaFai Lei <35485526+Will15800@users.noreply.github.com>
2024-09-08 06:27:52 +0000

    restore original layout

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,40 +1,34 @@
 import {
   Box,
   Card,
+  Spacer,
   Badge,
   CardBody,
   HStack,
   CardHeader,
+  CardFooter,
   Heading,
   Flex,
   Text,
 } from "@chakra-ui/react";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import EditProject from "./EditProject";
-import { PencilSquareIcon } from "@heroicons/react/24/outline";
 
 function ProjectCard({ project, onProjectUpdate }) {
-  function formatDate(dateString) {
-    const date = new Date(dateString);
-    const options = { year: "numeric", month: "short", day: "numeric" };
-
-    return date.toLocaleDateString("en-US", options);
-  }
-
   return (
     <Flex
       mt={"5vh"}
       flexDirection={"column"}
       justifyContent={"space-between"}
-      w={"40vh"}
-      h="23vh"
+      w={"44vh"}
+      h="22vh"
       bg={"#F4F4F4"}
     >
       <Card
         borderRadius="md"
         boxShadow="0 4px 6px rgba(0, 0, 0, 0.3)"
         h="full"
-        p={4}
+        p={2}
         bg="#F0F0F0"
       >
         <CardHeader
@@ -44,61 +38,52 @@ function ProjectCard({ project, onProjectUpdate }) {
           alignItems="center"
         >
           <Box>
-            <HStack>
-              <Heading size="md" mb={1} width={"160px"} isTruncated>
-                {project.project_name}
-              </Heading>
-              <Badge
-                bg={
-                  project.status === "In Progress"
-                    ? "#ff0000"
-                    : project.status === "Completed"
-                    ? "#48BB78"
-                    : "#A0AEC0"
-                }
-                color="white"
-                fontSize="small"
-                fontWeight="none"
-                display="flex"
-                borderRadius={"0.5vh"}
-                alignItems="center"
-                paddingLeft={"1vh"}
-                textTransform="none"
-              >
-                {project.status} <Box as="span" ml={1} className="bi bi-x" />
-              </Badge>
-            </HStack>
-            <Text fontSize={"13px"} w={"270px"} isTruncated>
-              Lead: {project.project_lead}
-            </Text>
+            <Heading size="md" mb={1}>
+              {project.project_name}
+            </Heading>
+            <Text>Lead: {project.project_lead}</Text>
           </Box>
+          <Badge
+            bg={
+              project.status === "In Progress"
+                ? "#4299E1"
+                : project.status === "Completed"
+                ? "#48BB78"
+                : "#A0AEC0"
+            }
+            color="white"
+            fontSize="small"
+            fontWeight="none"
+            display="flex"
+            borderRadius={"0.5vh"}
+            alignItems="center"
+            paddingLeft={"1vh"}
+            textTransform="none"
+          >
+            {project.status} <Box as="span" ml={1} className="bi bi-x" />
+          </Badge>
         </CardHeader>
-
-        <CardBody p={2} fontSize={"14px"} h={"75%"}>
-          <Text w="75%" textColor="#A39999" noOfLines={2}>
+        <CardBody p={2}>
+          <Text w="75%" textColor="#A39999">
             {project.description}
           </Text>
-          <HStack w="100%" marginTop={"10px"} gap={"70px"}>
-            <Text isTruncated>
+        </CardBody>
+        <CardFooter p={2}>
+          <HStack w="100%">
+            <Text>
               Created on:{" "}
               <Text textColor="#A39999" as="span">
-                {formatDate(project.created_at)}
+                {project.created_at}
               </Text>
             </Text>
+            
             <Spacer />
             <EditProject 
             project={project}
             onProjectUpdate={onProjectUpdate}
           />
           </HStack>
-        </CardBody>
-        <Flex
-          justifyContent={"flex-end"}
-          marginTop={"-25px"}
-          marginRight={"-4px"}
-        >
-          <PencilSquareIcon width={"20px"}></PencilSquareIcon>
-        </Flex>
+        </CardFooter>
       </Card>
     </Flex>
   );
